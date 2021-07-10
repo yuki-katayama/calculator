a988f1f4cefb48ffb10d273d3068b67c8ba3e7a4
yuki-katayama <katayu810@gmail.com>
2021-07-10 23:57:12 +0900

    fix * of bag and done invalid direct input

--- docs/index.js
+++ docs/index.js
@@ -99,10 +99,7 @@ function exec_m(v, html_v)
 	else if (v == 'M+')
 		dentaku.mem_m += dentaku.last_v;
 	else if (v == 'MC')
-	{
 		dentaku.mem_m = 0;
-		document.querySelector("input").value = dentaku.mem_m;
-	}
 	else if (v == 'MR')
 	{
 		dentaku.mem_not_m = html_v
