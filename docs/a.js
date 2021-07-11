e77ca43199ac1f953ba5ffbb9dcaa1a7f5e0da19
yuki-katayama <katayu810@gmail.com>
2021-07-11 17:56:45 +0900

    save

--- docs/a.js
+++
@@ -1,9 +0,0 @@
-a = ""
-// a = a.split(/[-+/* ]/);
-
-// if (a = (true == true) ? false : true)
-// console.log(a.split(/[-+/* ]/).slice(-1)[0].length, a.length)
-console.log(isFinite(3333334456))
-// b = 111
-// b /= 10
-// console.log(b)
