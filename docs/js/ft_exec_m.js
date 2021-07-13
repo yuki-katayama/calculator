55be8ec3759697854ff52d7ec0f136bf4042475e
yuki-katayama <katayu810@gmail.com>
2021-07-13 13:48:39 +0900

    fix m

--- docs/js/ft_exec_m.js
+++ docs/js/ft_exec_m.js
@@ -1,13 +1,23 @@
 function ft_exec_m(v, html_v) {
 	if (v === 'M-')
+	{
+		ft_append_oneline(`M-: ${dentaku.mem_m} - ${dentaku.last_v} = (MR)${dentaku.mem_m - dentaku.last_v}`)
 		dentaku.mem_m -= dentaku.last_v;
+	}
 	else if (v === 'M+')
+	{
+		ft_append_oneline(`M+: ${dentaku.mem_m} + ${dentaku.last_v} = (MR)${dentaku.mem_m + dentaku.last_v}`)
 		dentaku.mem_m += dentaku.last_v;
+	}
 	else if (v === 'MC')
+	{
+		ft_append_oneline(`MC: ${dentaku.mem_m}(MR) → 0`)
 		dentaku.mem_m = 0;
+	}
 	else if (v === 'MR' && !(dentaku.memflg)) {
 		dentaku.mem_not_m = html_v
 		document.querySelector("input").value = dentaku.mem_m;
 		dentaku.memflg = true
-	}
+	} else if (v === 'MR')
+		document.querySelector("input").value = dentaku.mem_m;
 }
